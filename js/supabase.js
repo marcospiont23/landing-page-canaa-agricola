7068bef664ca07b6ae9a43e8a1bfc7028c5eb285
marcospiont23 <marcosvinicius_lp@hotmail.com>
2026-09-24 21:34:59 -0300

    fix: alteração do caminho de deploy

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = 'https://sjoxvmfmofdyecqnzhfe.supabase.co';
-const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_xWnQ6DPz67SgKo3saBGwIA_HavE9v_Q';
+const SUPABASE_URL = 'https://wmvpzooubmaximjplcgt.supabase.co';
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_ac2q_kdmvI2d42oGQVdfHw_1PiFUQKt';
 
 window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
 	auth: {
